e19f1715858650d3585c2c6a35618caa97df47c5
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-20 15:14:37 +0530

    Create EXP10.cpp

---
+++ EXP10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void swap(int x, int y)
+{
+  int swap;
+  swap=x;
+  x=y;
+  y=swap;
+}
+
+int main() 
+{
+  int a=5,b=2;
+  swap(a,b);
+  cout<<"Value of a is: "<<a<<endl;
+  cout<<"Value of b is: "<<b<<endl;
+  return 0;
+}
